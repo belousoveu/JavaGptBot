a7aecd3973c36820820e73537038b5d37dbf2750
Belousoveu <belousov.eu@gmail.com>
2024-10-11 14:42:51 +0300

    Initial commit

---
+++ src/test/java/org/example/javagptbot/JavaGptBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javagptbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGptBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
